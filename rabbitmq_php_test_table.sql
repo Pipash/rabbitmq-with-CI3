6e63649ddd4f14ee907eb92f6c14443d75c86725
pipash <pipash.06@gmail.com>
2023-06-12 17:00:13 +0600

    database and table creation sql dumped

---
+++ rabbitmq_php_test_table.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 8.0.33-0ubuntu0.20.04.2 : Database - rabbitmq_php_test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`rabbitmq_php_test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `rabbitmq_php_test`;
+
+/*Table structure for table `test_table` */
+
+DROP TABLE IF EXISTS `test_table`;
+
+CREATE TABLE `test_table` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `data` varchar(255) DEFAULT NULL,
+  `insert_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17661 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
